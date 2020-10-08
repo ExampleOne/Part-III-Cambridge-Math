66669b82e8126e13a9b5c75a88712ddb4368d0ce
Quinten Tupker <qmt20@cam.ac.uk>
2020-10-08 10:42:41 +0100

    added QC lecture 1

---
+++ Michaelmas/auto/Quantum Computation.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "Quantum Computation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "report"
+    "rep10"
+    "inputenc"
+    "mathtools"
+    "amssymb"
+    "graphicx"
+    "listings"
+    "float"
+    "gensymb"
+    "amsthm"
+    "longtable"
+    "adjustbox"
+    "physics")
+   (LaTeX-add-environments
+    "definition"))
+ :latex)
+
