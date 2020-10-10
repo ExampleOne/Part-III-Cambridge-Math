abcc698e284cb7e65c149e534394dfb2059b2e8a
Quinten Tupker <qmt20@cam.ac.uk>
2020-10-10 16:45:57 +0100

    SFP 1

---
+++ Michaelmas/auto/Symmetries Fields and Particles.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "Symmetries Fields and Particles"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "mathtools"
+    "amssymb"
+    "graphicx"
+    "listings"
+    "float"
+    "gensymb"
+    "amsthm"
+    "longtable"
+    "adjustbox"
+    "physics")
+   (LaTeX-add-environments
+    "definition"
+    "proposition"))
+ :latex)
+
